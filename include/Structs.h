e3444bc3747eb2f179a90037f1f879838d383a00
Cuprum77 <blockland70@gmail.com>
2023-09-02 13:51:47 +0200

    added pico specific precompiler

--- include/Structs.h
+++ include/Structs.h
@@ -5,8 +5,10 @@ extern "C"
 {
 #endif
 
+// Only include on a pico
+#ifdef PICO_BUILD
 #include "hardware/spi.h"
-
+#endif
 
 typedef struct
 {
@@ -17,13 +19,53 @@ typedef struct
     int scl;
 
     bool pio;
+#ifdef PICO_BUILD
     spi_inst_t* spi_instance;
+#endif
     unsigned int baudrate;
 } display_spi_config_t;
+
+typedef struct
+{
+    int db0;
+    int db1;
+    int db2;
+    int db3;
+    int hsync;
+    int vsync;
+    int vclk;
+} display_piss_config_t;
+
+typedef struct
+{
+    int db0;
+    int db1;
+    int db2;
+    int db3;
+    int db4;
+    int db5;
+    int db6;
+    int db7;
+    int db8;
+    int db9;
+    int db10;
+    int db11;
+    int db12;
+    int db13;
+    int db14;
+    int db15;
+    int db16;
+    int db17;
+    int hs;
+    int vs;
+    int dclk;
+} display_rgb_config_t;
 
 typedef enum
 {
     DISPLAY_SPI,
+    DISPLAY_PISS,
+    DISPLAY_RGB
 } display_interface_t;
 
 
@@ -41,6 +83,8 @@ typedef struct
     unsigned int rotation;
 
     display_spi_config_t spi;
+    display_piss_config_t piss;
+    display_rgb_config_t rgb;
 } display_config_t;
 
 #ifdef __cplusplus
