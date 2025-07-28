b4f073351d41c7d10a0a07aab43106bece12db67
Cuprum77 <blockland70@gmail.com>
2025-07-28 04:54:27 +0200

    preliminary 8080 support, may break compatibility

--- src/hardware_driver/hardware_driver.hpp
+++ src/hardware_driver/hardware_driver.hpp
@@ -19,8 +19,8 @@ typedef enum
 typedef struct pio_spi_inst 
 {
     PIO pio;
-    uint sm;
-    uint cs_pin;
+    uint32_t sm;
+    uint32_t cs_pin;
 } pio_spi_inst_t;
 
 class hardware_driver
@@ -28,6 +28,7 @@ class hardware_driver
 public:
     hardware_driver(display_config_t* config);
     void init(void);
+    void reset(uint32_t time_ms);
 
     void writeData(uint8_t command, const uint8_t* data, size_t length);
     void setDataMode(uint8_t command);
@@ -37,26 +38,33 @@ private:
     // general stuff
     display_config_t* config;
     display_interface_t interface;
-    bool pioMode = false;   // SPI mode only
+    bool pioMode = false;   // hw mode only
 
     // dma stuff
-    uint dma_tx;
+    uint32_t dma_tx;
     dma_channel_config dma_config;
 
     // pio stuff
     PIO pio;
-    uint sm;
-    uint offset;
+    uint32_t sm;
+    uint32_t offset;
     float clkdiv;
 
     // general
     bool enabled = false;
+    bool parallel_interface_in_sequence = false;
+    bool parallel_interface_reverse_order = false;
+    uint32_t parallel_interface_mask = 0;
+    uint32_t parallel_interface_min_pin = 0;
+    uint32_t parallel_interface_max_pin = 0;
 
     // private functions
     void initSPI(void);
     void initSPIwPIO(void);
-    void initPiss(void);
-    void initRGB(void);
+    void init8080(void);
     void changeSPIbits(spi_bit_length_t bits);
+    void write8080(uint32_t data, bool command, bool bit16);
     inline void setSPIdataCommandPins(bool dc, bool cs);
+    bool isSequential(uint32_t* arr, size_t size);
+    uint16_t flipBits(uint16_t value);
 };
