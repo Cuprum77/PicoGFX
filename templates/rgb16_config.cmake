5029704378c2e24e9e80830fbfb2133e823a4406
Cuprum77 <blockland70@gmail.com>
2026-03-17 23:56:24 +0100

    added files for the new rgb model

--- templates/rgb16_config.cmake
+++ templates/rgb16_config.cmake
@@ -14,6 +14,8 @@ set(LCD_PROTOCOL        PARALLEL_16)
 set(LCD_HARDWARE        RGB)
 # Set the color depth of the display (1, 8, 16, 18, or 24 bits per pixel)
 set(LCD_COLOR_DEPTH     ?)
+# What memory to store the framebuffer in (SRAM or PSRAM or NONE), most use SRAM
+set(LCD_FB_MEMORY       SRAM)
 
 # Set the width of the display in pixels
 set(LCD_WIDTH           ?)
@@ -21,14 +23,8 @@ set(LCD_WIDTH           ?)
 set(LCD_HEIGHT          ?)
 # Set the rotation of the display (0, 90, 180, or 270 degrees)
 set(LCD_ROTATION        ?)
-# Offset in pixels from the left side of the display
-set(LCD_OFFSET_X0       ?)
-# Offset in pixels from the right side of the display
-set(LCD_OFFSET_X1       ?)
-# Offset in pixels from the top of the display
-set(LCD_OFFSET_Y0       ?)
-# Offset in pixels from the bottom of the display
-set(LCD_OFFSET_Y1       ?)
+# LCD pixel clock in Hz, e.g. 25000000 for 25 MHz
+set(LCD_DCLK_FREQ       ?)
 
 # Set the pin number for the pins used to connect the display to the microcontroller
 set(LCD_PIN_DB0         ?)
@@ -51,6 +47,7 @@ set(LCD_PIN_DE          ?)
 set(LCD_PIN_VSYNC       ?)
 set(LCD_PIN_HSYNC       ?)
 set(LCD_PIN_PCLK        ?)
+set(LCD_PIN_EN          ?)
 set(LCD_PIN_RST         ?)
 
 # If the backlight is inverted (i.e. LOW turns on the backlight, HIGH turns off the backlight), set this to 1, otherwise set to 0
